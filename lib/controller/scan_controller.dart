cfaaa302421829261b5817978d52100e904f00a5
ameyagidh <ameyagidh2@gmail.com>
2023-10-20 21:39:15 -0400

    done

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:get/state_manager.dart';
 import 'package:permission_handler/permission_handler.dart';
+import "package:flutter_tflite/flutter_tflite.dart";
 
 class ScanController extends GetxController {
   @override
@@ -8,6 +9,7 @@ class ScanController extends GetxController {
     // TODO: implement onInit
     super.onInit();
     initCamera();
+    initTFLite();
   }
 
   @override
@@ -18,8 +20,11 @@ class ScanController extends GetxController {
   }
 
   late CameraController cameraController;
+
   late List<CameraDescription> cameras;
+  var cameraCount = 0;
   var isCameraInitialized = false.obs;
+
   initCamera() async {
     if (await Permission.camera.request().isGranted) {
       cameras = await availableCameras();
@@ -27,11 +32,50 @@ class ScanController extends GetxController {
         cameras[0],
         ResolutionPreset.max,
       );
-      await cameraController.initialize();
+      await cameraController.initialize().then((value){
+        cameraController.startImageStream((image){
+          cameraCount +=1 ;
+          if(cameraCount % 10 == 0){
+            cameraCount = 0;
+            objectDetector(image);
+          }
+          update();
+        });
+        
+      });
       isCameraInitialized(true);
       update();
     } else {
       print("Permission Denied");
+    }
+  }
+
+  initTFLite() async{
+    await Tflite.loadModel(
+      model: "assets/model.tflite",
+      labels: "assets/labels.txt",
+      isAsset: true,
+      numThreads: 1,
+      useGpuDelegate: false,
+    );
+  }
+
+  objectDetector(CameraImage image) async{
+    var detector = await Tflite.runModelOnFrame(
+        bytesList: image.planes.map((e){
+          return e.bytes;
+        }).toList(),
+        asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      rotation: 90,
+      threshold: 0.4,
+    );
+    if(detector != null){
+      print("object detected is $detector");
     }
   }
 }
